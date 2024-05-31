a895d4c494515925ee79f6f35047b6bf77ef513c
Fotis Gimian <fgimiansoftware@gmail.com>
2024-05-31 18:08:11 +1000

    Reuse the data buffer when reading files to avoid excessive allocations.

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -71,6 +72,8 @@ var rootCmd = &cobra.Command{
 		pluginCounts32 := make(map[parser.Plugin]int)
 		pluginCounts64 := make(map[parser.Plugin]int)
 
+		var projectBytes []byte
+
 		for _, projectPath := range args {
 			err := filepath.Walk(
 				projectPath,
@@ -89,7 +92,7 @@ var rootCmd = &cobra.Command{
 						}
 					}
 
-					projectBytes, err := os.ReadFile(path)
+					projectBytes, err = readFileIntoSlice(path, projectBytes)
 					if err != nil {
 						return nil
 					}
@@ -213,5 +216,54 @@ func printSummary(pluginCounts map[parser.Plugin]int, description string, headin
 	for _, plugin := range plugins {
 		count := pluginCounts[plugin]
 		fmt.Printf("    > %s : %s (%d)\n", plugin.GUID, plugin.Name, count)
+	}
+}
+
+func readFileIntoSlice(name string, data []byte) ([]byte, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var size int
+	if info, err := f.Stat(); err == nil {
+		size64 := info.Size()
+		if int64(int(size64)) == size64 {
+			size = int(size64)
+		}
+	}
+	size++ // one byte for final read at EOF
+
+	// If a file claims a small size, read at least 512 bytes.
+	// In particular, files in Linux's /proc claim size 0 but
+	// then do not work right if read in small pieces,
+	// so an initial read of 1 byte would not work correctly.
+	if size < 512 {
+		size = 512
+	}
+
+	// Grow the buffer if needed.
+	if size -= cap(data); size > 0 {
+		data = append(data[:cap(data)], make([]byte, size)...)
+	}
+
+	// Clear the buffer.
+	data = data[:0]
+
+	for {
+		n, err := f.Read(data[len(data):cap(data)])
+		data = data[:len(data)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return data, err
+		}
+
+		if len(data) >= cap(data) {
+			d := append(data[:cap(data)], 0)
+			data = d[:len(data)]
+		}
 	}
 }
